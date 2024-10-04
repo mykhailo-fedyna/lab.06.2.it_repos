0cd405adb591bdc32392146c09c97a76e22852b6
misha-fedyna <mihajlofedina5@gmail.com>
2024-10-04 17:19:48 +0300

    first master commit

---
+++ Lab6.2(it).cpp
@@ -0,0 +1,46 @@
+﻿// lab6.2(it).cpp
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+void Create(int* r, const int size, const int Low, const int High)
+{
+	for (int i = 0; i < size; i++)
+		r[i] = Low + rand() % (High - Low + 1);
+}
+void Print(int* r, const int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << setw(4) << r[i];
+	cout << endl;
+}
+int Max(int* a, const int size)
+{
+	int max = a[0];
+	for (int i = 1; i < size; i++)
+		if (a[i] % 2 == 0 && a[i] > max)
+			max = a[i];
+	return max;
+}
+int MaxIndex(int* a, const int size)
+{
+	int max = Max(a, size);
+	for (int i = 0; i < size; i++)
+		if (a[i] == max)
+			return i;
+	return -1;
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 30;
+	int r[n];
+	int Low = 1;
+	int High = 100;
+	Create(r, n, Low, High);
+	Print(r, n);
+	cout << "Max = " << Max(r, n) << endl;
+	cout << "Index of Max = " << MaxIndex(r, n) << endl;
+
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab6.2(it).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1_MaxEven)
+		{
+			const int n = 6;
+			int arr[n] = { 3, 10, 2, 5, 8, 1 };
+
+			int expectedMax = 10;
+			int actualMax = Max(arr, n);
+
+			Assert::AreEqual(expectedMax, actualMax);
+		}
+
+		TEST_METHOD(TestMethod2_MaxEvenIndex)
+		{
+			const int n = 6;
+			int arr[n] = { 3, 10, 2, 5, 8, 1 };
+
+			int expectedIndex = 1;
+			int actualIndex = MaxIndex(arr, n);
+
+			Assert::AreEqual(expectedIndex, actualIndex);
+		}
+
+		TEST_METHOD(TestMethod3_NoEvenNumbers)
+		{
+			const int n = 5;
+			int arr[n] = { 3, 9, 7, 5, 1 };
+
+			int expectedIndex = 0;
+			int actualIndex = MaxIndex(arr, n);
+
+			Assert::AreEqual(expectedIndex, actualIndex);
+		}
+	};
+}
